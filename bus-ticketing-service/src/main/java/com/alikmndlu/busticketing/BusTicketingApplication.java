abccf009de86db4a74de1dfa14cc2b5483bf8fe6
Ali Kmndlu <alikmndlu1@gmail.com>
2021-10-23 11:48:05 +0330

    init project

---
+++ bus-ticketing-service/src/main/java/com/alikmndlu/busticketing/BusTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.alikmndlu.busticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusTicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusTicketingApplication.class, args);
+    }
+
+}
